82e4bdbe72f5f4df67c049e522b0244ed3b207c8
Maxence Schroeder <54377206+Maxence-schroeder01@users.noreply.github.com>
2021-11-18 16:02:48 -0500

    Resolution bddDao

--- donnees/BaseDeDonneesDAO.js
+++ donnees/BaseDeDonneesDAO.js
@@ -14,25 +14,16 @@ class BaseDeDonneesDAO {
         try{
             
             const xhttp = new XMLHttpRequest();
-            xhttp.onload =  function() {
-                console.log(this.responseText);
-                // this.coordonnees = this.responseText;
-            }
-                xhttp.onreadystatechange = async function(){
-                    if(xhttp.readyState === 4){
-                        this.coordonnees = await xhttp.responseText;
-                        console.log(this.coordonnees);
-                    }
-                    console.log(this.coordonnees);
-
-                }
-
-                xhttp.open("GET", "http://51.222.140.74:3000");
+                xhttp.open("GET", "http://51.222.140.74:3000",false);
                 xhttp.send();
+                console.log(xhttp.responseText);
+                this.coordonnees = xhttp.responseText;
+                
             }
             catch(err){
                 console.log(err.stack)
             }
+            return this.coordonnees;
 
     }
 
