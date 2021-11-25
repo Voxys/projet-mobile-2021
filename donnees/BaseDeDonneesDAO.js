0483b48827031e5d218e9082256b636bfc2a6e16
Voxys <50014072+Voxys@users.noreply.github.com>
2021-11-25 12:47:57 -0500

    Nettoyage

--- donnees/BaseDeDonneesDAO.js
+++ donnees/BaseDeDonneesDAO.js
@@ -6,30 +6,28 @@ class BaseDeDonneesDAO {
 
     async obtenirCoordonnees(idSentier) {
 
-        try{
-                const xhttp = new XMLHttpRequest();
-                xhttp.open("GET", "http://51.222.140.74:3000",false);
-                xhttp.setRequestHeader("id", idSentier);
-                xhttp.setRequestHeader("requete", "obtenirCoordonnees");
-                xhttp.send();
-                this.coordonnees = xhttp.responseText;
-            }
-            catch(err){
-                console.log(err.stack)
-            }
-            return this.coordonnees;
+        try {
+            const xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "http://51.222.140.74:3000", false);
+            xhttp.setRequestHeader("id", idSentier);
+            xhttp.setRequestHeader("requete", "obtenirCoordonnees");
+            xhttp.send();
+            this.coordonnees = xhttp.responseText;
+        } catch (err) {
+            console.log(err.stack)
+        }
+        return this.coordonnees;
     }
 
-    async obtenirListeSentiers(){
-        try{
+    async obtenirListeSentiers() {
+        try {
             const xhttp = new XMLHttpRequest();
-            xhttp.open("GET", "http://51.222.140.74:3000",false);
+            xhttp.open("GET", "http://51.222.140.74:3000", false);
             // xhttp.setRequestHeader("id", idSentier);
             xhttp.setRequestHeader("requete", "obtenirListeSentiers");
             xhttp.send();
             this.listeSentiers = xhttp.responseText;
-        }
-        catch(err){
+        } catch (err) {
             console.log(err.stack)
         }
         return this.listeSentiers;
--- vues/VueTableauBord.js
+++ vues/VueTableauBord.js
@@ -3,92 +3,40 @@ class VueTableauBord {
         this.tableauBord = document.getElementById("vue-tableau-bord").innerHTML;
         this.baseDeDonneesDAO = new BaseDeDonneesDAO();
     }
-    
+
     async afficher() {
         var liste = JSON.parse(await this.baseDeDonneesDAO.obtenirListeSentiers());
         document.getElementsByTagName("body")[0].innerHTML = this.tableauBord;
 
         document.getElementById("slides-container").innerHTML =
-        '<ion-slides id="slider">' + '</ion-slides>';
-        
-        for(let i = 0; i < liste.length; i++){
-            
+            '<ion-slides id="slider">' + '</ion-slides>';
+
+        for (let i = 0; i < liste.length; i++) {
+
             console.log(liste[i].properties);
 
-            var card =  
-            "<ion-slide>" +
-            '<a id="refSentier" href="#vue-carte/{Sentier.id}">' + 
-            "<ion-card>" +
-            "<ion-card-header>" +
-            "<ion-card-title>" + liste[i].properties['nom_etab'] + "</ion-card-title>" +
-            "</ion-card-header>" +
-            '<div class="ion-card-bottom">' +
-            '<ion-chip color="primary">' +
-            "<ion-label>" + liste[i].properties['usager'] + "</ion-label>" +
-            "</ion-chip>" +
-            "</div>" +
-            "</ion-card>" +
-            "</a>" +
-            "</ion-slide>";
+            var card =
+                "<ion-slide>" +
+                '<a id="refSentier" href="#vue-carte/{Sentier.id}">' +
+                "<ion-card>" +
+                "<ion-card-header>" +
+                "<ion-card-title>" + liste[i].properties['nom_etab'] + "</ion-card-title>" +
+                "</ion-card-header>" +
+                '<div class="ion-card-bottom">' +
+                '<ion-chip color="primary">' +
+                "<ion-label>" + liste[i].properties['usager'] + "</ion-label>" +
+                "</ion-chip>" +
+                "</div>" +
+                "</ion-card>" +
+                "</a>" +
+                "</ion-slide>";
 
-        document.getElementById("slider").insertAdjacentHTML('beforeend', card);
-        
-        
-    }
+            document.getElementById("slider").insertAdjacentHTML('beforeend', card);
+        }
 
-        for(let i = 0; i < liste.length; i++){
-            console.log("ICI" + liste[i].properties['fid']);
+        for (let i = 0; i < liste.length; i++) {
             let html = document.getElementById("slides-container").innerHTML;
             document.getElementById("slides-container").innerHTML = html.replace("{Sentier.id}", liste[i].properties['fid']);
         }
-
-        // var listeSentier = document.getElementById("slides-container");
-        // console.log(listeSentier);
-        // const listeSentierItemHTML = listeSentier.innerHTML;
-        // let listeSentierHTMLRemplacement = "";
-
-        // for(let i = 0; i < 1; i++){
-        //     let listeSentierHTMLRemplacement = listeSentierItemHTML;
-        //     listeSentierHTMLRemplacement = listeSentierItemHTML.replace("{Sentier.id}", liste[i].properties['fid']);
-        //     listeSentierHTMLRemplacement += listeSentierItemHTML;
-        //     console.log("======================================");
-
-        //     console.log(listeSentierHTMLRemplacement);
-        // }
-        // listeSentier.innerHTML = listeSentierHTMLRemplacement;
-        
-        
-        // document.getElementById("slides-container").innerHTML =
-        // '<ion-slides id="slider">' + '</ion-slides>';
-        
-        // for(let i = 0; i < liste.length; i++){
-            
-        //     console.log(liste[i].properties);
-
-        //     var card =  
-        //     "<ion-slide>" +
-        //     '<a id="refSentier" href="#vue-carte/{Sentier.id}">' + 
-        //     "<ion-card>" +
-        //     "<ion-card-header>" +
-        //     "<ion-card-title>" + liste[i].properties['nom_etab'] + "</ion-card-title>" +
-        //     "</ion-card-header>" +
-        //     '<div class="ion-card-bottom">' +
-        //     '<ion-chip color="primary">' +
-        //     "<ion-label>" + liste[i].properties['usager'] + "</ion-label>" +
-        //     "</ion-chip>" +
-        //     "</div>" +
-        //     "</ion-card>" +
-        //     "</a>" +
-        //     "</ion-slide>";
-
-        // document.getElementById("slider").insertAdjacentHTML('beforeend', card);
-        // document.getElementById("slider").innerHTML = card;
-        
-        
-    //}
-    
-
-        
-
     }
 }
