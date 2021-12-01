99c5d1761d9855db61b8a38ef8e7a7d69ec3959c
Voxys <50014072+Voxys@users.noreply.github.com>
2021-12-01 17:01:33 -0500

    Intégration Android Studio

---
+++ app/src/main/assets/www/vues/VueCarte.js
@@ -0,0 +1,73 @@
+class VueCarte {
+	constructor() {
+		this.vueCarte = document.getElementById("vue-carte").innerHTML;
+		this.dao = new SentierDAO();
+		this.map;
+		this.sentier;
+	}
+
+	afficher() {
+		document.getElementsByTagName("body")[0].innerHTML = this.vueCarte;
+	}
+
+	async initialiserCarte(id) {
+
+		const positionMatane = {
+			lat: 48.849998,
+			lng: -67.533333
+		};
+
+		this.map = new google.maps.Map(document.getElementById("map"), {
+			mapId: "8752fd40d51bd45c",
+			disableDefaultUI: true,
+			center: {
+				lat: positionMatane.lat,
+				lng: positionMatane.lng
+			},
+			zoom: 15,
+		});
+		this.sentier = await this.dao.initialiserCoordonneesSentier(id);
+		var path = this.dao.tracerSentier(this.sentier);
+		var marqueurDebutSentier = this.dao.ajouterMarqueur(this.sentier[0], "Debut sentier", "purple-dot");
+		var marqueurFinSentier = this.dao.ajouterMarqueur(this.sentier[this.sentier.length - 1], "Fin sentier", "flag");
+
+		path.setMap(this.map);
+		marqueurDebutSentier.setMap(this.map);
+		marqueurFinSentier.setMap(this.map);
+
+		this.map.setCenter(this.sentier[0]);
+
+		//Attendre le clique du bouton pour lancer l'itinéraire
+		this.itineraireSentier();
+	}
+
+	itineraireSentier() {
+		let button = document.getElementById("start");
+		button.addEventListener("click", () => {
+
+			var directionsService = new google.maps.DirectionsService();
+			var directionsRenderer = new google.maps.DirectionsRenderer();
+			directionsRenderer.setMap(this.map);
+
+			console.log(this.sentier[0]);
+
+			var request = {
+				origin:  new google.maps.LatLng(48.45850041730631, -68.49808773840113),
+				destination: new google.maps.LatLng(this.sentier[0].lat, this.sentier[0].lng),
+				// Note that JavaScript allows us to access the constant
+				// using square brackets and a string value as its
+				// "property."
+				travelMode: 'DRIVING'
+			};
+			directionsService.route(request, function (response, status) {
+				if (status == 'OK') {
+					directionsRenderer.setDirections(response);
+				}
+			});
+
+
+		})
+	}
+}
+// origin: new google.maps.LatLng(48.849998,-67.533333),
+// destination: new google.maps.LatLng(48.4525,-68.5232),
